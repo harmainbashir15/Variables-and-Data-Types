eac7cf19eb46fc08d75be2e22ce72bba14dbaedc
Harmain Bashir <162583900+harmainbashir15@users.noreply.github.com>
2024-03-10 04:44:02 -0700

    Add files via upload

---
+++ variables.ts
@@ -0,0 +1,78 @@
+let username: string = " Harmain Fatima"; 
+console.log(username)
+let education: string = "Intermediate";
+console.log(education)
+let nationalatiy: string = " Pakistan";
+console.log(nationalatiy)
+let age: number =  19;
+console.log(age)
+let height: number = " 5'4 "
+console.log(height)
+let weight: number = 51;
+console.log(weight)
+let haircolor: string = "Brown";
+console.log(haircolor)
+let eyecolor: string = "Brown";
+console.log(eyecolor)
+let favcolor: string = "Black";
+console.log(favcolor)
+let favoritefruit: string =  "Mango , orange , Strawberry";
+console.log(favoritefruit)
+let favdish: string = "Biryani";
+console.log(favdish)
+
+let favoritecountry: string = "Turkey , Maldives , Bali , Europe , SaudiArabia";
+console.log(favoritecountry)
+let luckeynum: number = 6;
+console.log(luckeynum)
+
+let thingsilike: string = "Moon , Sunset , Sky , long walk"; 
+console.log(thingsilike)
+
+let myhobby: string = "Cooking , Sleeping , Reading Books";
+console.log(myhobby)
+let favmovie: string = "Tangled";
+console.log(favmovie)
+let favfootballplayer: string = "cristiano Ronaldo";
+console.log(favfootballplayer)
+let favoritecricketer: string = "Shahid Khan Afridi , Shane Watson , AB de Villiers";
+console.log(favoritecricketer)
+let single: boolean = true;
+console.log(single)
+let graduated: boolean = false;
+console.log(graduated) 
+let currentbalance = 2000   // this value can change
+console.log(currentbalance)
+const accountnumber = "123456789" // this value remains constant  
+console.log(accountnumber)
+let link: any = "https://www.linkedin.com/in/harmain-fatima/"; // any: for variable that can hold any type of data.
+console.log(link)
+
+let firstname: string = "Harmain"
+let lastname: string =  "Fatima"
+let fullname: string = `${firstname} ${lastname}`;   // template literals
+console.log(fullname)
+let favcar: string = "Fortuner , land Cruiser , Lamborghini"
+console.log(favcar)
+let siblings: string = "We are 7 siblings, 5 brothers & 2 sisters";
+console.log(siblings)
+let isstudent: boolean = "true";
+console.log(isstudent)
+let whichcityyoulive: string = "Digri";
+console.log(whichcityyoulive)
+let isstrawberryred: boolean = "true";
+console.log(isstrawberryred)
+let mybirthday: string = "15 October"
+console.log(mybirthday)
+let favoriteflower: string = "Red Rose , Tulip"
+console.log(favoriteflower)
+let mostusedappinmyphone: string = "Instagram"
+console.log(mostusedappinmyphone)
+let zodiacsign: string = "Libra"
+console.log(zodiacsign)
+
+
+
+
+
+
